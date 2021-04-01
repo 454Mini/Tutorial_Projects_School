5417bae14692d997ad3b838b0730604b38af280a
Henriette Bjerre Vandt <henr15r3@edu.ucl.dk>
2021-04-01 17:42:48 +0200

    20 %

---
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BananaService:IBananaService
+    {
+        public event Action OnChange;
+        
+        public int Bananas { get; set; } = 1000;
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananasChanged();
+        }
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananasChanged();
+        }
+
+        public void BananasChanged() => OnChange.Invoke();
+        //Invokes event
+
+       
+    }
+}
